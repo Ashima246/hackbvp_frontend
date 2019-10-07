81267765bfd8fac11b3b319545032ac87790216b
Ashima246 <ashima2499@gmail.com>
2019-10-07 23:38:22 +0530

    third commit

--- scripts/backend.js
+++ scripts/backend.js
@@ -1,38 +1,123 @@
 $(() => {
 
-// var slider = document.getElementById("myRange1");
-// var output = document.getElementById("demo");
-// output.innerHTML = slider.value;
 
-// slider.oninput = function() {
-// output.innerHTML = this.value;
-// }
 
-var config = {
-    apiKey: "...",
-    authDomain: "...",
-    databaseURL: "...",
-    projectId: "...",
-    storageBucket: "...",
-    messagingSenderId: "...."
-  };
-  firebase.initializeApp(config);
+// var config = {
+//     apiKey: "...",
+//     authDomain: "...",
+//     databaseURL: "...",
+//     projectId: "...",
+//     storageBucket: "...",
+//     messagingSenderId: "...."
+//   };
+//   firebase.initializeApp(config);
 
 $(".slider").change(function(event){
-    
+    console.log($("#myRangeH").val());
     let values = {
         history : $("#myRangeH").val(),
         entertainment : $("#myRangeE").val(),
         arts : $("#myRangeA").val(),
         nature : $("#myRangeN").val(),
         relaxation : $("#myRangeR").val(),
-        food : $("#myRangeF").val()
-
+        food : $("#myRangeF").val(),
     }
+    $.post(
+        //url,
+        values,
+        (data) => {
+            $(".one1").each((i,v) => {
+                $(v).append (`<img src="`+ data.cqImagePath +`" alt="">`);
+                // $(v).append (data.cqImage.image);
+                // data -> json Object
+                // cqImage -> id of the image for code quest.
+                // image-> attribute int the table
+            })
 
-    var xttp = new XMLHttpRequest();
-    xttp.open("POST", "", true);
-    xttp.send(values);
+            $(".two2").each((i,v) => {
+                $(v).append (`<img src="`+ data.cqImagePath +`" alt="">`);
+                // $(v).append (data.cqImage.image);
+                // data -> json Object
+                // cqImage -> id of the image for code quest.
+                // image-> attribute int the table
+            })
+
+            $(".three3").each((i,v) => {
+                $(v).append (`<img src="`+ data.cqImagePath +`" alt="">`);
+                // $(v).append (data.cqImage.image);
+                // data -> json Object
+                // cqImage -> id of the image for code quest.
+                // image-> attribute int the table
+            })
+            $(".four4").each((i,v) => {
+                $(v).append (`<img src="`+ data.cqImagePath +`" alt="">`);
+                // $(v).append (data.cqImage.image);
+                // data -> json Object
+                // cqImage -> id of the image for code quest.
+                // image-> attribute int the table
+            })
+            $(".five5").each((i,v) => {
+                $(v).append (`<img src="`+ data.cqImagePath +`" alt="">`);
+                // $(v).append (data.cqImage.image);
+                // data -> json Object
+                // cqImage -> id of the image for code quest.
+                // image-> attribute int the table
+            })
+            $(".six6").each((i,v) => {
+                $(v).append (`<img src="`+ data.cqImagePath +`" alt="">`);
+                // $(v).append (data.cqImage.image);
+                // data -> json Object
+                // cqImage -> id of the image for code quest.
+                // image-> attribute int the table
+            })
+            $(".seven7").each((i,v) => {
+                $(v).append (`<img src="`+ data.cqImagePath +`" alt="">`);
+                // $(v).append (data.cqImage.image);
+                // data -> json Object
+                // cqImage -> id of the image for code quest.
+                // image-> attribute int the table
+            })
+            $(".eight8").each((i,v) => {
+                $(v).append (`<img src="`+ data.cqImagePath +`" alt="">`);
+                // $(v).append (data.cqImage.image);
+                // data -> json Object
+                // cqImage -> id of the image for code quest.
+                // image-> attribute int the table
+            })
+            $(".nine9").each((i,v) => {
+                $(v).append (`<img src="`+ data.cqImagePath +`" alt="">`);
+                // $(v).append (data.cqImage.image);
+                // data -> json Object
+                // cqImage -> id of the image for code quest.
+                // image-> attribute int the table
+            })
+            $(".ten10").each((i,v) => {
+                $(v).append (`<img src="`+ data.cqImagePath +`" alt="">`);
+                // $(v).append (data.cqImage.image);
+                // data -> json Object
+                // cqImage -> id of the image for code quest.
+                // image-> attribute int the table
+            })
+            $(".eleven11").each((i,v) => {
+                $(v).append (`<img src="`+ data.cqImagePath +`" alt="">`);
+                // $(v).append (data.cqImage.image);
+                // data -> json Object
+                // cqImage -> id of the image for code quest.
+                // image-> attribute int the table
+            })
+
+
+        }
+    )
+    });
+
+    // data = {
+    //     cqurl : ""
+    // }
+
+    // var xttp = new XMLHttpRequest();
+    // xttp.open("POST", "", true);
+    // xttp.send(values);
 
 
 
@@ -95,7 +180,4 @@ $(".slider").change(function(event){
         
     }
     )*/
-})
-
-
 })
